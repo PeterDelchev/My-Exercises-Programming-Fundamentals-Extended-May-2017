3ab4e3f4303ef123b07733bdee1c021a797b8509
Peter Delchev <delchev_pv@yahoo.com>
2017-09-15 18:50:21 +0300

    commit

---
+++ Programming Fundamentals 23.05.2017/4. CSharp Intro and Basic Syntax/Problem 3. Employee Data/Problem 3. Employee Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Problem_3.Employee_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            int employeeID = int.Parse(Console.ReadLine());
+            double monthlySalary = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {employeeID:d8}");
+            Console.WriteLine($"Salary: {monthlySalary:f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals 23.05.2017/4. CSharp Intro and Basic Syntax/Problem 4. Beverage Labels/Problem 4. Beverage Labels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problem_4.Beverage_Labels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyPer100ml = int.Parse(Console.ReadLine());
+            int sugarPer100ml = int.Parse(Console.ReadLine());
+
+            double energy = energyPer100ml * volume / 100.00;
+            double sugar = sugarPer100ml * volume / 100.00;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
