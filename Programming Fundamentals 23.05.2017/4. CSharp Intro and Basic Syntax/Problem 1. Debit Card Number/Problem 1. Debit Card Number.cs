b5bffaab20cc09bde8ff3e7200fe5b53cd05faa4
Peter Delchev <delchev_pv@yahoo.com>
2017-09-15 17:37:48 +0300

    new commit

---
+++ Programming Fundamentals 23.05.2017/4. CSharp Intro and Basic Syntax/Problem 1. Debit Card Number/Problem 1. Debit Card Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Problem_1.Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+            int forth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{first:d4} {second:d4} {third:d4} {forth:d4}");
+
+
+
+        }
+    }
+}
